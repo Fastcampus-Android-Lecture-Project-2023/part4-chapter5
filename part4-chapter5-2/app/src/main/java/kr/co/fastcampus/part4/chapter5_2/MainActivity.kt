d9452225e108fe072beb180d58015826a8ab47e4
LYK <dalinaum@gmail.com>
2023-02-27 17:03:47 +0900

    뷰 모델 파이널

--- part4-chapter5-2/app/src/main/java/kr/co/fastcampus/part4/chapter5_2/MainActivity.kt
+++ part4-chapter5-2/app/src/main/java/kr/co/fastcampus/part4/chapter5_2/MainActivity.kt
@@ -35,27 +35,14 @@ class MainActivity : ComponentActivity() {
 
 // 단계 2: `ViewModel`을 상속받은 `ToDoViewModel`을 만듭니다.
 // 첫 단계에서는 내용을 비워두고 시작합시다.
-
-// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
-// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
-// 에러가 발생하면 아래의 `import` 문을 추가합니다.
-// `import androidx.lifecycle.viewmodel.compose.viewModel`
-@Composable
-fun TopLevel() {
-    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
-    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
-    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
-    // `remember`는 제거해야 합니다.
-    val (text, setText) = remember { mutableStateOf("") }
-
-    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
-    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
-    val toDoList = remember { mutableStateListOf<ToDoData>() }
+class ToDoViewModel() : ViewModel() {
+    var text by mutableStateOf("")
+    val toDoList = mutableStateListOf<ToDoData>()
 
     val onSubmit: (String) -> Unit = {
         val key = (toDoList.lastOrNull()?.key ?: 0) + 1
         toDoList.add(ToDoData(key, it))
-        setText("")
+        text = ""
     }
 
     val onEdit: (Int, String) -> Unit = { key, newText ->
@@ -72,24 +59,64 @@ fun TopLevel() {
         val i = toDoList.indexOfFirst { it.key == key }
         toDoList.removeAt(i)
     }
+}
+
+// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
+// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
+// 에러가 발생하면 아래의 `import` 문을 추가합니다.
+// `import androidx.lifecycle.viewmodel.compose.viewModel`
+@Composable
+fun TopLevel(viewModel: ToDoViewModel = viewModel()) {
+    // 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
+    // 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
+    // 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
+    // `remember`는 제거해야 합니다.
+//    val (text, setText) = remember { mutableStateOf("") }
+
+    // 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
+    // `onDelete`를 모두 뷰 모델로 옮겨봅시다.
+//    val toDoList = remember { mutableStateListOf<ToDoData>() }
+
+//    val onSubmit: (String) -> Unit = {
+//        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+//        toDoList.add(ToDoData(key, it))
+//        viewModel.text = ""
+//    }
+//
+//    val onEdit: (Int, String) -> Unit = { key, newText ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList[i] = toDoList[i].copy(text = newText)
+//    }
+//
+//    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList[i] = toDoList[i].copy(done = checked)
+//    }
+//
+//    val onDelete: (Int) -> Unit = { key ->
+//        val i = toDoList.indexOfFirst { it.key == key }
+//        toDoList.removeAt(i)
+//    }
 
     Scaffold {
         Column {
             ToDoInput(
-                text = text,
-                onTextChange = setText,
-                onSubmit = onSubmit
+                text = viewModel.text,
+                onTextChange = {
+                    viewModel.text = it
+                },
+                onSubmit = viewModel.onSubmit
             )
             LazyColumn {
                 items(
-                    items = toDoList,
+                    items = viewModel.toDoList,
                     key = { it.key }
                 ) { toDoData ->
                     ToDo(
                         toDoData = toDoData,
-                        onEdit = onEdit,
-                        onToggle = onToggle,
-                        onDelete = onDelete
+                        onEdit = viewModel.onEdit,
+                        onToggle = viewModel.onToggle,
+                        onDelete = viewModel.onDelete
                     )
                 }
             }
